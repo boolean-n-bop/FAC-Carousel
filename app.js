ef33d727b970e2489bd34cb1c5f455fe5d92d303
Meltem Ozmen <meltem.o@hotmail.co.uk>
2020-04-25 12:13:51 +0100

    Carousel Code

---
+++ app.js
@@ -0,0 +1,86 @@
+ 
+var slideIndex,slides;
+function beginSlides(){
+  slideIndex=0;
+  slides=document.getElementsByClassName("slide");
+  slides[slideIndex].style.opacity=1;
+  
+  if(slides.length<2){
+        var nextPrevBtns=document.querySelector(".leftArrow,.rightArrow");
+        nextPrevBtns.style.display="none";
+        for (i = 0; i < nextPrevBtn.length; i++) {
+            nextPrevBtn[i].style.display="none";
+        }
+    }
+}
+
+beginSlides();
+
+function plusSlides(n){
+   moveSlide(slideIndex+n);
+}
+
+function moveSlide(n){
+    var i,current,next;
+    var moveSlideAnimClass={
+          forCurrent:"",
+          forNext:""
+    }
+    if(n>slideIndex) {
+        if(n >= slides.length){n=0}
+        moveSlideAnimClass.forCurrent="moveLeftCurrentSlide";
+        moveSlideAnimClass.forNext="moveLeftNextSlide";
+    }else if(n<slideIndex){
+        if(n<0){n=slides.length-1}
+        moveSlideAnimClass.forCurrent="moveRightCurrentSlide";
+        moveSlideAnimClass.forNext="moveRightPrevSlide";
+    }
+      if(n!=slideIndex){
+        next = slides[n];
+        current=slides[slideIndex];
+        for (i = 0; i < slides.length; i++) {
+            slides[i].className = "slide";
+            slides[i].style.opacity=0;
+        }
+        current.classList.add(moveSlideAnimClass.forCurrent);
+        next.classList.add(moveSlideAnimClass.forNext);
+        slideIndex=n;
+       
+    }
+     
+}
+
+
+//automatic scrolling
+var timer=null;
+function setTimer(){
+    timer=setInterval(function () {
+        plusSlides(1) ;
+    },4500);
+}
+setTimer();
+function playPauseSlides() {
+    var playBtn=document.getElementById("play");
+    var pauseBtn=document.getElementById("pause")
+    if(timer==null){
+        setTimer();
+        pauseBtn.style.opacity=1;
+        playBtn.style.opacity=0;
+    }else{
+        clearInterval(timer);
+        timer=null;
+        playBtn.style.opacity=1;
+        pauseBtn.style.opacity=0;
+    }
+}
+
+
+//event listener to enable keyboad navigation
+document.addEventListener("keydown", function(e) {
+         if (e.keyCode == '37') {
+            document.getElementsByClassName("arrowLeft")[0].click();
+         }
+         else if (e.keyCode == '39') {
+             document.getElementsByClassName("arrowRight")[0].click();
+         }
+     });
